c0d62265205856ab8068ff11c8a7ba4aa46269a7
miho yamagata <miho.yamagata@gmail.com>
2021-02-03 00:08:22 +0900

    modify App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ function App() {
   // 分かりやすいように'margin: 50px'を適用しています。//
   return (
     <div className="App" style={{ margin: 50 }}>
+      <Auth> 
+        {/* ログインしていたら */}
+        {(authInfo) => {
+          if (authInfo.isLoggedIn) {
+
+          // ログアウトしていたら
+          } else {
+
+          }
+        }}
       <Chat />
+      </Auth>
     </div>
   );
 }
