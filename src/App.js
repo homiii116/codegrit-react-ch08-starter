ad40a64f8b040d737b047307e337932aee198a30
miho yamagata <miho.yamagata@gmail.com>
2021-02-03 22:49:52 +0900

    uploaded

--- src/App.js
+++ src/App.js
@@ -7,19 +7,27 @@ import LogoutLink from './components/LogoutLink';
 function App() {
   // 分かりやすいように'margin: 50px'を適用しています。//
   return (
-    <div className="App" style={{ margin: 50 }}>
-      <Auth> 
+    <div className="App" style={{ margin: 50 }}> 
+      <Auth>      
         {/* ログインしていたら */}
         {(authInfo) => {
           if (authInfo.isLoggedIn) {
-
+            const { me, logout } = authInfo;
+            console.log(authInfo);
+            return  (
+              <>
+                <LogoutLink me={me} logout={logout} /> 
+                <Chat />
+              </>
+           )
           // ログアウトしていたら
           } else {
-
+            const { login } = authInfo;
+            console.log(authInfo);
+            return <LoginPage login={login} />
           }
-        }}
-      <Chat />
-      </Auth>
+        }}  
+      </Auth>   
     </div>
   );
 }
